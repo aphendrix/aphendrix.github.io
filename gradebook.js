039142e7d0717d6da885fc89a4d5e4e0d43c67de
aphendrix <aphendrix@my.waketech.edu>
2025-04-28 22:09:16 -0400

    Created gradebook.html and gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,10 @@
+function fetchGradeData() {
+    console.log("fetchGradeData function called");
+}
+
+function populateGradebook() {
+    console.log("populateGradebook function called");
+}
+
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
